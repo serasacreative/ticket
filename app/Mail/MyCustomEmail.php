1295ba7190f09ad2bfe99bcfd748af56beff1dc4
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-27 00:27:50 +0700

    p

--- app/Mail/MyCustomEmail.php
+++ app/Mail/MyCustomEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class MyCustomEmail extends Mailable implements ShouldQueue
+class MyCustomEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
