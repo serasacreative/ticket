cf22cbc538e9f73bc5b6ed9f458b286228ecb9b3
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-26 23:36:49 +0700

    fix

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -40,7 +40,7 @@ class TicketController extends Controller
         //SAMPLE REQUEST START HERE
 
         // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = config('midtranc.server_key');
+        \Midtrans\Config::$serverKey = config('midtrans.server_key');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
         \Midtrans\Config::$isProduction = false;
         // Set sanitization on (default)
