1d89d3fe6a6a93a67b2ad23ab32e49894970acce
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-27 12:21:28 +0700

    katanya fix

--- resources/views/ticket/scan.blade.php
+++ resources/views/ticket/scan.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-    
     <script src="{{ asset('quagga.min.js') }}"></script>
-    
     <style>
         #barcode-scanner {
             width: 100%;
@@ -18,18 +16,7 @@
 
 @section('content')
     <div class="container-fluid">
-
-        <div class="block-header py-lg-4 py-3">
-            <div class="row g-3">
-                <div class="col-md-6 col-sm-12">
-                    <h2 class="m-0 fs-5"><a href="javascript:void(0);" class="btn btn-sm btn-link ps-0 btn-toggle-fullwidth"><i class="fa fa-arrow-left"></i></a>Tickets</h2>
-                    <ul class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="index.html">Lucid</a></li>
-                        <li class="breadcrumb-item active">Ticket</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <!-- Your existing content here -->
 
         <div class="row clearfix">
             <div class="col-12">
@@ -38,7 +25,7 @@
                         <h6 class="card-title">Order Your Ticket</h6>
                     </div>
                     <div class="card-body">
-                        <h1> Scan Ticket</h1>
+                        <h1>Scan Ticket</h1>
                         <div class="row w-100 d-flex justify-content-evenly">
                             <div class="card py-4 text-center">
                                 <div class="card-body">
@@ -47,102 +34,88 @@
                                     <button type="button" class="btn btn-outline-primary" id="stop-button">Stop</button>
                                 </div>
                             </div>
-                            
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
-
 @endsection
+
 @section('js')
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        // Flag to track if the scanner is running
-        let scannerRunning = false;
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            let scannerRunning = false;
 
-        // Barcode scanning function
-        function scanBarcode() {
-            if (!scannerRunning) {
-                scannerRunning = true; // Set the flag to true
+            function scanBarcode() {
+                if (!scannerRunning) {
+                    scannerRunning = true;
+                    Quagga.init({
+                        inputStream: {
+                            name: "Live",
+                            type: "LiveStream",
+                            target: document.querySelector("#barcode-scanner"),
+                            constraints: {
+                                facingMode: "environment" // Use the rear camera for mobile devices
+                            },
+                        },
+                        decoder: {
+                            readers: ["code_128_reader"], // Specify the barcode format to scan (e.g., CODE128)
+                            debug: {
+                                drawBoundingBox: true,
+                                showFrequency: true,
+                                drawScanline: true,
+                                showPattern: true,
+                            },
+                        },
+                    }, function (err) {
+                        if (err) {
+                            console.error("Error initializing Quagga:", err);
+                            scannerRunning = false;
+                            return;
+                        }
+                        Quagga.start();
+                    });
 
-                Quagga.start();
+                    Quagga.onDetected(function (result) {
+                        scannerRunning = false;
+                        Quagga.stop();
+                        if (result && result.codeResult && result.codeResult.code) {
+                            const barcodeValue = result.codeResult.code;
+
+                            $.ajax({
+                                url: "{{ route('ticket.verify') }}",
+                                type: "POST",
+                                data: { barcode: barcodeValue },
+                                headers: {
+                                    "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+                                },
+                                success: function (response) {
+                                    Swal.fire({
+                                        icon: 'success',
+                                        title: 'Ticket successfully scanned!',
+                                        text: 'Quantity: ' + response.qty,
+                                    });
+                                },
+                                error: function (xhr, status, error) {
+                                    console.error("Error sending AJAX request:", error);
+                                },
+                            });
+                        }
+                    });
+                }
             }
-        }
 
-        // Initialize Quagga
-        Quagga.init({
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                target: document.querySelector("#barcode-scanner"),
-                constraints: {
-                    facingMode: "environment" // Use the rear camera for mobile devices
-                },
-            },
-            decoder: {
-                readers: ["code_128_reader"], // Specify the barcode format to scan (e.g., CODE128)
-                debug: {
-                    drawBoundingBox: true,
-                    showFrequency: true,
-                    drawScanline: true,
-                    showPattern: true,
-                },
-            },
-        }, function (err) {
-            if (err) {
-                console.error("Error initializing Quagga:", err);
-                return;
-            }
-            // Attach the processed event listener
-            Quagga.onProcessed(function (result) {
-                if (result && result.codeResult && result.codeResult.code) {
-                    // Barcode detected, stop scanning and process only once
-                    if (scannerRunning) {
-                        scannerRunning = false; // Reset the flag
+            document.getElementById("scan-button").addEventListener("click", function () {
+                scanBarcode();
+            });
 
-                        const barcodeValue = result.codeResult.code;
-
-                        // Send AJAX request to your Laravel backend
-                        $.ajax({
-                            url: "{{ route('ticket.verify') }}",
-                            type: "POST",
-                            data: { barcode: barcodeValue },
-                            headers: {
-                                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
-                            },
-                            success: function (response) {
-                                console.log(response)
-                                Swal.fire({
-                                    icon: 'success',
-                                    title: 'Ticket successfully scanned !',
-                                    text: 'Quantity : ' + response.qty,
-                                });
-                            },
-                            error: function (xhr, status, error) {
-                                console.error("Error sending AJAX request:", error);
-                            },
-                        });
-                    }
+            document.getElementById("stop-button").addEventListener("click", function () {
+                if (scannerRunning) {
+                    Quagga.stop();
+                    scannerRunning = false;
                 }
             });
         });
-
-        // Attach the scanning function to the "Scan" button click event
-        document.getElementById("scan-button").addEventListener("click", function () {
-            scanBarcode();
-        });
-
-        document.getElementById("stop-button").addEventListener("click", function () {
-            if (scannerRunning) {
-                Quagga.stop();
-                scannerRunning = false; // Reset the flag when stopping the scanner manually
-            }
-        });
-    });
-</script>
+    </script>
 @endsection
-
-
