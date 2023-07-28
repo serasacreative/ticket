43adda6da66696c5f6a8d9efd88ed2bd97472888
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-28 08:47:18 +0700

    lists

--- resources/views/ticket/vip_soldout.blade.php
+++ resources/views/ticket/vip_soldout.blade.php
@@ -20,7 +20,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h6 class="card-title">Order Your Ticket</h6>
+                        <h6 class="card-title">Order Your Tickets</h6>
                     </div>
                     <div class="card-body">
                         <h1> Ticket List</h1>
