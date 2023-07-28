0257587a60b620e2afb65c9ef9366eea5c52942c
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-28 09:22:31 +0700

    y

---
+++ resources/views/emails/my_custom_email.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Email Subject</title>
+</head>
+<body>
+    <div style="text-align: center;">
+    <img src="https://ticket.muallem.id/banner_mail.jpg" alt="Banner Image" style="max-width: 100%;">
+        
+    </div>
+    <p>Klick this <a href="{{$url}}">link</a> to generate barcode</p><br />
+    <p>Thank you for using our service!</p>
+
+
+</body>
+</html>
---
+++ resources/views/ticket/generate.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Barcode Ticket</title>
+    <style>
+        @page {
+            size: auto;
+            margin: 0mm;
+        }
+    </style>
+</head>
+
+<body>
+    <svg id="barcode"></svg>
+</body>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="{{ asset('assets/plugin/barcodegen/JsBarcode.all.min.js') }}"></script>
+<script>
+    $(function() {
+        $("#barcode").JsBarcode("{{$ticket->bar_code}}", {
+            width: 1.5,
+            height: 65,
+            fontSize: 26,
+            format: "code128",
+            displayValue: true
+        });
+    })
+</script>
+<script>
+
+</script>
+
+</html>
