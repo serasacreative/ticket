59c09ddf47b73fb2f5579387376e1262271459ce
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-28 08:39:22 +0700

    no ignore

--- resources/views/ticket/festival_soldout.blade.php
+++ resources/views/ticket/festival_soldout.blade.php
@@ -37,7 +37,7 @@
                                         </ul>
                                         <p class="fs-2">Rp 100.000</p>
 
-                                        <img src="{{asset('/sol-out.png')}}" class="img-fluid"/>
+                                        <img src="{{asset('/sold-out.png')}}" class="img-fluid"/>
                                     </div>
                                 </div>
                             </div>
