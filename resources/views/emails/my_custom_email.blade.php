b1df2eda6cafb43159cc6129cff812c3e6a48a3a
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-28 17:18:49 +0700

    barcodemail

--- resources/views/emails/my_custom_email.blade.php
+++ resources/views/emails/my_custom_email.blade.php
@@ -7,10 +7,25 @@
     <div style="text-align: center;">
     <img src="https://ticket.serasacreative.com/banner_mail.jpg" alt="Banner Image" style="max-width: 100%;">
         
+    total order = {{$ticket->qty}} 
+    <svg id="barcode"></svg>
     </div>
-    <p>Klick this <a href="{{$url}}">link</a> to generate barcode</p><br />
-    <p>Thank you for using our service!</p>
+    
 
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="{{ asset('assets/plugin/barcodegen/JsBarcode.all.min.js') }}"></script>
+    <script>
+        $(function() {
+            // $("#barcode").JsBarcode("{{$ticket->bar_code}}", {
+            $("#barcode").JsBarcode("{{$ticket->bar_code}}", {
+                width: 1.5,
+                height: 65,
+                fontSize: 26,
+                format: "code128",
+                displayValue: true
+            });
+        })
+    </script>
 </body>
 </html>
