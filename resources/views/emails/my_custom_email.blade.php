bbd0daac8070f72b37c0fc2902ca97afab9124a7
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 13:04:44 +0700

    bg

--- resources/views/emails/my_custom_email.blade.php
+++ resources/views/emails/my_custom_email.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<div style="text-align: center;">
+<div style="text-align: center; background-color: #fff">
     <div style="border: 1px solid #ced4da; border-radius: 0.25rem; max-width: 400px; margin: 0 auto;">
         <div style="background-color: #f8f9fa; padding: 0.75rem 1.25rem;">
             <h6 style="margin: 0; font-size: 1.125rem; font-weight: bold;">Informasi Tiket</h6>
@@ -16,7 +16,7 @@
         <div style="padding: 1.25rem;">
             <ul style="list-style: none; padding: 0;">
                 <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Nama : {{$ticket->name}} </li>
-                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Kategori : {{ strtoupper($ticket->categori) }}</li>
+                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Kategori : {{ $ticket->categori }}</li>
                 <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Jumlah Tiket : {{$ticket->qty}}</li>
                 <li style="padding: 0.5rem 0;">
                     <p style="color: #17a2b8; margin: 0;">Dapatkan tiket anda dengan klik</p> 
