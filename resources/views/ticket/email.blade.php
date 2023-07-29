fde57a1eb1d5923e3bcc378c856a6a4a9ad1c1ec
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 12:48:55 +0700

    oi

--- resources/views/ticket/email.blade.php
+++ resources/views/ticket/email.blade.php
@@ -10,15 +10,19 @@
         <div class="card">
             <div class="card-header">
                 <h6 class="card-title">Informasi Tiket</h6>
+                <img class="img-fluid img-thumbnail w-50 mx-auto" src="{{asset('Logo-Mahafest.png')}}" alt="">
             </div>
             <div class="card-body">
                 <ul class="list-group list-group-custom">
                     <li class="list-group-item">Nama : aku </li>
                     <li class="list-group-item">Kategori : VIP</li>
                     <li class="list-group-item">Jumlah Tiket : 3</li>
+                    <li class="list-group-item">
+                        <div class="d-flex justify-content-center"><p class="bg-light">Dapatkan tiket anda dengan klik</p> <a href="https://ticket.serasacreative.com/ticket/generate/" class="btn btn-primary" type="button"> Tiket Saya </a></div>
+                    </li>
                 </ul>
             </div>
-            <div ><p class="bg-light">Dapatkan tiket anda dengan klik</p> <a href="https://ticket.serasacreative.com/ticket/generate/" class="btn btn-primary" type="button"> Tiket Saya </a></div>
+            
         </div>
 </body>
 
