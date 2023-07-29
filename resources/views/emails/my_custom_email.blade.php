e681e5acb9a6d668d032c150b72d28005f55419b
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 11:09:05 +0700

    try barcode

--- resources/views/emails/my_custom_email.blade.php
+++ resources/views/emails/my_custom_email.blade.php
@@ -4,33 +4,51 @@
     <title>Your Email Subject</title>
 
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+
+    <!-- Include the canvg library -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/canvg/1.5/canvg.min.js"></script>
 </head>
 <body>
     <div style="text-align: center;">
-    <img src="https://ticket.serasacreative.com/banner_mail.jpg" alt="Banner Image" style="max-width: 100%;">
-        
+        <img src="https://ticket.serasacreative.com/banner_mail.jpg" alt="Banner Image" style="max-width: 100%;">
     </div>
     
     Nama : {{$ticket->name}} 
     Kategori : {{$ticket->name}} 
     Jumlah Tiket : {{$ticket->qty}} 
-    <svg id="barcode"></svg>
 
+    <!-- Add a canvas element to render the barcode -->
+    <canvas id="barcodeCanvas"></canvas>
 
-</body>
+    <!-- Your other HTML content -->
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="{{ asset('assets/plugin/barcodegen/JsBarcode.all.min.js') }}"></script>
-<script>
-    $(function() {
-        // $("#barcode").JsBarcode("{{$ticket->bar_code}}", {
-        $("#barcode").JsBarcode("{{$ticket->bar_code}}", {
-            width: 1.5,
-            height: 65,
-            fontSize: 26,
-            format: "code128",
-            displayValue: true
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="{{ asset('assets/plugin/barcodegen/JsBarcode.all.min.js') }}"></script>
+    <script>
+        $(function() {
+            // Generate the barcode SVG
+            var barcodeSvg = JsBarcode.getBarcodeSvg("{{$ticket->bar_code}}", {
+                width: 1.5,
+                height: 65,
+                fontSize: 26,
+                format: "CODE128",
+                displayValue: true
+            });
+
+            // Convert the SVG to a PNG image using canvg
+            var canvas = document.getElementById("barcodeCanvas");
+            canvg(canvas, barcodeSvg);
+
+            // Extract the image data from the canvas
+            var imageData = canvas.toDataURL("image/png");
+
+            // Create an <img> element with the barcode image data
+            var barcodeImage = document.createElement("img");
+            barcodeImage.src = imageData;
+
+            // Append the image to the email body
+            document.body.appendChild(barcodeImage);
         });
-    })
-</script>
+    </script>
+</body>
 </html>
