1dc54ff9b8aef89b0ae796118994718383e8492a
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 17:22:13 +0700

    hgjhads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>:: Ticket</title>
+    <title>Ticket MahaFest</title>
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Ticket MahaFest">
