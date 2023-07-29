302be342be91724ecb9aed42472c4092b2d5c80d
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 13:00:31 +0700

    fix

--- resources/views/emails/my_custom_email.blade.php
+++ resources/views/emails/my_custom_email.blade.php
@@ -6,20 +6,26 @@
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 </head>
 <body>
-    <div style="text-align: center;">
-        <div class="card">
-            <div class="card-header">
-                <h6 class="card-title">Informasi Tiket</h6>
-            </div>
-            <div class="card-body">
-                <ul class="list-group list-group-custom">
-                    <li class="list-group-item">Nama : {{$ticket->name}} </li>
-                    <li class="list-group-item">Kategori : {{$ticket->name}}</li>
-                    <li class="list-group-item">Jumlah Tiket : {{$ticket->qty}}</li>
-                </ul>
-            </div>
-            <div class="alert alert-info" role="alert">Dapatkan tiket anda dengan klik <a href="https://ticket.serasacreative.com/ticket/generate/{{$url}}" class="btn btn-primary text-decoration-none text-white" > Tiket Saya </a></div>
+
+<div style="text-align: center;">
+    <div style="border: 1px solid #ced4da; border-radius: 0.25rem; max-width: 400px; margin: 0 auto;">
+        <div style="background-color: #f8f9fa; padding: 0.75rem 1.25rem;">
+            <h6 style="margin: 0; font-size: 1.125rem; font-weight: bold;">Informasi Tiket</h6>
+            <img style="max-width: 50%; display: block; margin: 10px auto;" src="{{asset('Logo-Mahafest.png')}}" alt="">
         </div>
+        <div style="padding: 1.25rem;">
+            <ul style="list-style: none; padding: 0;">
+                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Nama : {{$ticket->name}} </li>
+                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Kategori : {{ strtoupper($ticket->categori) }}</li>
+                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Jumlah Tiket : {{$ticket->qty}}</li>
+                <li style="padding: 0.5rem 0;">
+                    <p style="color: #17a2b8; margin: 0;">Dapatkan tiket anda dengan klik</p> 
+                    <a href="https://ticket.serasacreative.com/ticket/generate/{{$url}}" style="background-color: #007bff; color: #fff; text-decoration: none; padding: 0.5rem 1rem; border-radius: 0.25rem; display: inline-block; margin-top: 5px;">Tiket Saya</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 </body>
 
 </html>
--- resources/views/ticket/email.blade.php
+++ resources/views/ticket/email.blade.php
@@ -1,24 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div style="text-align: center;">
-    <div style="border: 1px solid #ced4da; border-radius: 0.25rem; max-width: 400px; margin: 0 auto;">
-        <div style="background-color: #f8f9fa; padding: 0.75rem 1.25rem;">
-            <h6 style="margin: 0; font-size: 1.125rem; font-weight: bold;">Informasi Tiket</h6>
-            <img style="max-width: 50%; display: block; margin: 10px auto;" src="{{asset('Logo-Mahafest.png')}}" alt="">
-        </div>
-        <div style="padding: 1.25rem;">
-            <ul style="list-style: none; padding: 0;">
-                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Nama : aku </li>
-                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Kategori : VIP</li>
-                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Jumlah Tiket : 3</li>
-                <li style="padding: 0.5rem 0;">
-                    <p style="color: #17a2b8; margin: 0;">Dapatkan tiket anda dengan klik</p> 
-                    <a href="{{route('ticket.vip')}}" style="background-color: #007bff; color: #fff; text-decoration: none; padding: 0.5rem 1rem; border-radius: 0.25rem; display: inline-block; margin-top: 5px;">Tiket Saya</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
 
 @endsection
