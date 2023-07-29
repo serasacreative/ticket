598edf7647e29b80f4a0b12d9406baee6c4e0347
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 11:20:43 +0700

    p

--- app/Mail/MyCustomEmail.php
+++ app/Mail/MyCustomEmail.php
@@ -8,12 +8,14 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Milon\Barcode\DNS1D;
 
 class MyCustomEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
     public $order_id;
+
     /**
      * Create a new message instance.
      */
@@ -31,10 +33,20 @@ class MyCustomEmail extends Mailable implements ShouldQueue
     {
         $order_id = $this->order_id;
         $ticket = Ticket::find($order_id);
+
+        // Generate the barcode image
+        $barcode = new DNS1D();
+        $barcode->setStorPath(public_path('temp')); // Set the temporary storage path
+        $barcodeImage = $barcode->getBarcodePNG($ticket->bar_code, 'C128');
+
         return $this->view('emails.my_custom_email')
             ->subject('Ticket Notification')
+            ->attachData($barcodeImage, 'barcode.png', [
+                'mime' => 'image/png',
+            ])
             ->with([
                 'ticket' => $ticket
             ]);
     }
 }
+
--- resources/views/emails/my_custom_email.blade.php
+++ resources/views/emails/my_custom_email.blade.php
@@ -4,9 +4,6 @@
     <title>Your Email Subject</title>
 
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
-
-    <!-- Include the canvg library -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/canvg/1.5/canvg.min.js"></script>
 </head>
 <body>
     <div style="text-align: center;">
@@ -17,38 +14,11 @@
     Kategori : {{$ticket->category}} 
     Jumlah Tiket : {{$ticket->qty}} 
 
-    <!-- Add a canvas element to render the barcode -->
-    <canvas id="barcodeCanvas"></canvas>
-
     <!-- Your other HTML content -->
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="{{ asset('assets/plugin/barcodegen/JsBarcode.all.min.js') }}"></script>
-    <script>
-        $(function() {
-            // Generate the barcode SVG
-            var barcodeSvg = JsBarcode.getBarcodeSvg("{{$ticket->bar_code}}", {
-                width: 1.5,
-                height: 65,
-                fontSize: 26,
-                format: "CODE128",
-                displayValue: true
-            });
+    <!-- Display the barcode image -->
+    <img src="{{ $message->embedData($barcodeImage, 'barcode.png') }}" alt="Barcode Image" style="display: block; margin: 0 auto;">
 
-            // Convert the SVG to a PNG image using canvg
-            var canvas = document.getElementById("barcodeCanvas");
-            canvg(canvas, barcodeSvg);
-
-            // Extract the image data from the canvas
-            var imageData = canvas.toDataURL("image/png");
-
-            // Create an <img> element with the barcode image data
-            var barcodeImage = document.createElement("img");
-            barcodeImage.src = imageData;
-
-            // Append the image to the email body
-            document.body.appendChild(barcodeImage);
-        });
-    </script>
+    <!-- Your other HTML content -->
 </body>
 </html>
