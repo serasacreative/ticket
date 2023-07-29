8032906cd5a4839f9b49593256106e842b323480
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 16:46:02 +0700

    console

--- resources/views/ticket/scan.blade.php
+++ resources/views/ticket/scan.blade.php
@@ -31,35 +31,36 @@
         if (barcode.endsWith('\n')) {
             // Remove the newline character
             const trimmedBarcode = barcode.trim();
+            console.log(trimmedBarcode)
 
-            // Make an AJAX request to the Laravel route using jQuery
-            $.ajax({
-                url: '/scan-barcode',
-                method: 'POST',
-                data: { barcode: trimmedBarcode },
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                success: function(response) {
-                    // Show SweetAlert with the response message
-                    Swal.fire({
-                        title: 'Success',
-                        text: response.message,
-                        icon: 'success',
-                    }).then(() => {
-                        // After clicking 'OK', refocus on the barcode input field for automatic scanning
-                        document.getElementById('barcodeInput').focus();
-                    });
-                },
-                error: function(error) {
-                    // Show SweetAlert with error message
-                    Swal.fire({
-                        title: 'Error',
-                        text: 'Error processing barcode data',
-                        icon: 'error',
-                    });
-                }
-            });
+            // // Make an AJAX request to the Laravel route using jQuery
+            // $.ajax({
+            //     url: '/scan-barcode',
+            //     method: 'POST',
+            //     data: { barcode: trimmedBarcode },
+            //     headers: {
+            //         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            //     },
+            //     success: function(response) {
+            //         // Show SweetAlert with the response message
+            //         Swal.fire({
+            //             title: 'Success',
+            //             text: response.message,
+            //             icon: 'success',
+            //         }).then(() => {
+            //             // After clicking 'OK', refocus on the barcode input field for automatic scanning
+            //             document.getElementById('barcodeInput').focus();
+            //         });
+            //     },
+            //     error: function(error) {
+            //         // Show SweetAlert with error message
+            //         Swal.fire({
+            //             title: 'Error',
+            //             text: 'Error processing barcode data',
+            //             icon: 'error',
+            //         });
+            //     }
+            // });
 
             // Clear the input field after processing
             document.getElementById('barcodeInput').value = '';
