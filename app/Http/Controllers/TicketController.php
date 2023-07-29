6219e7eb3ec0a233e249406c90e17282bce7a6ad
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 16:28:08 +0700

    scan

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -308,10 +308,10 @@ class TicketController extends Controller
 
     public function verify(Request $request)
     {
-        $ticket = Ticket::where('bar_code', $request->barcode)->where('status', '<>', 'scanned')->first();
+        $ticket = Ticket::where('bar_code', $request->barcode)->first();
         if($ticket){
-            $ticket->status = 'scanned';
-            $ticket->save();
+            // $ticket->status = 'scanned';
+            // $ticket->save();
             return response()->json(['valid' => true, 'data' => $ticket]);
         }   
         return response()->json(['valid' => false]);
--- resources/views/ticket/scan.blade.php
+++ resources/views/ticket/scan.blade.php
@@ -22,17 +22,13 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h6 class="card-title">Order Your Ticket</h6>
+                        <h6 class="card-title">Ticket Scanner</h6>
                     </div>
                     <div class="card-body">
-                        <h1>Scan Ticket</h1>0
+                        <h1>Scanning</h1>0
                         <div class="row w-100 d-flex justify-content-evenly">
-                            <div class="card py-4 text-center">
-                                <div class="w-100">
-                                    <div id="barcode-scanner"></div>
-                                    <button type="button" class="btn btn-outline-primary" id="scan-button">Scan</button>
-                                    <button type="button" class="btn btn-outline-primary" id="stop-button">Stop</button>
-                                </div>
+                            <div class="card py-4 text-center"> 
+                                <input type="text" id="barcodeInput" autofocus>
                             </div>
                         </div>
                     </div>
@@ -41,90 +37,55 @@
         </div>
     </div>
 @endsection
-@section('js')
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            let scannerRunning = false;
-
-            function scanBarcode() {
-                if (!scannerRunning) {
-                    scannerRunning = true;
-                    Quagga.init({
-                        inputStream: {
-                            name: "Live",
-                            type: "LiveStream",
-                            target: document.querySelector("#barcode-scanner"),
-                            constraints: {
-                                facingMode: "environment" // Use the rear camera for mobile devices
-                            },
-                        },
-                        decoder: {
-                            readers: ["code_128_reader"], // Specify the barcode format to scan (e.g., CODE128)
-                            debug: {
-                                drawBoundingBox: true,
-                                showFrequency: true,
-                                drawScanline: true,
-                                showPattern: true,
-                            },
-                        },
-                    }, function (err) {
-                        if (err) {
-                            console.error("Error initializing Quagga:", err);
-                            scannerRunning = false;
-                            return;
-                        }
-                        Quagga.start();
-                    });
-
-                    Quagga.onDetected(function (result) {
-                        if (scannerRunning) {
-                            scannerRunning = false;
-                            if (result && result.codeResult && result.codeResult.code) {
-                                const barcodeValue = result.codeResult.code;
-
-                                $.ajax({
-                                    url: "{{ route('ticket.verify') }}",
-                                    type: "POST",
-                                    data: { barcode: barcodeValue },
-                                    headers: {
-                                        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
-                                    },
-                                    success: function (response) {
-                                        console.log(response)
-                                        if (response.valid) {
-                                            Swal.fire({
-                                                icon: 'success',
-                                                title: 'Ticket successfully scanned!',
-                                                text: 'Quantity: ' + response.data.qty,
-                                            });
-                                        } else {
-                                            // Handle the case when the barcode is not valid
-                                            // For example, show an error message
-                                            console.log("Invalid barcode");
-                                            // Do not stop Quagga, let it continue scanning
-                                        }
-                                        scannerRunning = true;
-                                    },
-                                    error: function (xhr, status, error) {
-                                        console.error("Error sending AJAX request:", error);
-                                    },
-                                });
-                            }
-                        }
-                    });
-                }
+@section('js')<script>
+    // Function to handle the AJAX request
+    function sendBarcodeData(barcode) {
+        // Make an AJAX request to the Laravel route using jQuery
+        $.ajax({
+            url: "{{route('ticket.verify')}}",
+            method: 'POST',
+            data: { barcode: barcode },
+            success: function(response) {
+                // Show SweetAlert with the response message
+                console.log(response)
+                Swal.fire({
+                    title: 'Success',
+                    text: response.message,
+                    icon: 'success',
+                }).then(() => {
+                    // After clicking 'OK', refocus on the barcode input field for automatic scanning
+                    document.getElementById('barcodeInput').focus();
+                });
+            },
+            error: function(error) {
+                // Show SweetAlert with error message
+                Swal.fire({
+                    title: 'Error',
+                    text: 'Error processing barcode data',
+                    icon: 'error',
+                });
             }
-
-            document.getElementById("scan-button").addEventListener("click", function () {
-                scanBarcode();
-            });
-
-            document.getElementById("stop-button").addEventListener("click", function () {
-                    Quagga.stop();
-                    scannerRunning = false;
-                
-            });
         });
-    </script>
+    }
+
+    // Event listener for the barcode input field
+    document.getElementById('barcodeInput').addEventListener('input', function(event) {
+        // Get the barcode input value
+        const barcode = event.target.value;
+
+        // Assuming the scanner appends a newline character at the end of the scan
+        if (barcode.endsWith('\n')) {
+            // Remove the newline character
+            const trimmedBarcode = barcode.trim();
+
+            // Call the function to handle the barcode data
+            sendBarcodeData(trimmedBarcode);
+
+            // Clear the input field after processing
+            event.target.value = '';
+        }
+    });
+</script>
+
 @endsection
 
