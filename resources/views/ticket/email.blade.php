6347ab099dd51fc23485be5202841c578e4363f4
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 12:57:59 +0700

    inline

--- resources/views/ticket/email.blade.php
+++ resources/views/ticket/email.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="text-align: center;">
-        <div class="card">
-            <div class="card-header">
-                <h6 class="card-title">Informasi Tiket</h6>
-                <img class="img-fluid img-thumbnail w-50 mx-auto mt-2" src="{{asset('Logo-Mahafest.png')}}" alt="">
-            </div>
-            <div class="card-body">
-                <ul class="list-group list-group-custom">
-                    <li class="list-group-item">Nama : aku </li>
-                    <li class="list-group-item">Kategori : VIP</li>
-                    <li class="list-group-item">Jumlah Tiket : 3</li>
-                    <li class="list-group-item">
-                        <p class="text-info">Dapatkan tiket anda dengan klik</p> 
-                        <a href="{{route('ticket.vip')}}" class="btn btn-primary">Tiket Saya</a>
-                    </li>
-                </ul>
-            </div>
-            
+<div style="text-align: center;">
+    <div style="border: 1px solid #ced4da; border-radius: 0.25rem; max-width: 400px; margin: 0 auto;">
+        <div style="background-color: #f8f9fa; padding: 0.75rem 1.25rem;">
+            <h6 style="margin: 0; font-size: 1.125rem; font-weight: bold;">Informasi Tiket</h6>
+            <img style="max-width: 50%; display: block; margin: 10px auto;" src="{{asset('Logo-Mahafest.png')}}" alt="">
         </div>
+        <div style="padding: 1.25rem;">
+            <ul style="list-style: none; padding: 0;">
+                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Nama : aku </li>
+                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Kategori : VIP</li>
+                <li style="border-bottom: 1px solid #ced4da; padding: 0.5rem 0;">Jumlah Tiket : 3</li>
+                <li style="padding: 0.5rem 0;">
+                    <p style="color: #17a2b8; margin: 0;">Dapatkan tiket anda dengan klik</p> 
+                    <a href="{{route('ticket.vip')}}" style="background-color: #007bff; color: #fff; text-decoration: none; padding: 0.5rem 1rem; border-radius: 0.25rem; display: inline-block; margin-top: 5px;">Tiket Saya</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
 @endsection
