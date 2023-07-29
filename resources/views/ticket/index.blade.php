b09d8652bc03b1e26a6858efd19fe87b6aa4fd0b
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-07-29 12:27:11 +0700

    w50

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -38,7 +38,7 @@
                             <div class="col-lg-4 col-md-5">
                                 <div class="card py-4 text-center">
                                     <div class="card-body">
-                                        <img class="img-fluid img-thumbnail" src="{{asset('Logo-Mahafest.png')}}" alt="">
+                                        <img class="img-fluid img-thumbnail w-50 mx-auto" src="{{asset('Logo-Mahafest.png')}}" alt="">
                                         <h2 class="pricing-header">VIP</h2>
                                         <ul class="pricing-features list-unstyled">
                                             <li class="my-2">Free Merchandise</li>
@@ -59,7 +59,7 @@
                             <div class="col-lg-4 col-md-5">
                                 <div class="card py-4 text-center">
                                     <div class="card-body">
-                                        <img class="img-fluid img-thumbnail" src="{{asset('Logo-Mahafest.png')}}" alt="">
+                                        <img class="img-fluid img-thumbnail w-50 mx-auto" src="{{asset('Logo-Mahafest.png')}}" alt="">
                                         <h2 class="pricing-header">Festival</h2>
                                         <ul class="pricing-features list-unstyled">
                                             <li class="my-2">Free Merchandise</li>
