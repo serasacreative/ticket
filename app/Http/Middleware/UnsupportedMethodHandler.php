d69e456d69a321baafba2beea44e6d39f15c075d
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-08-01 07:56:50 +0700

    h

--- app/Http/Middleware/UnsupportedMethodHandler.php
+++ app/Http/Middleware/UnsupportedMethodHandler.php
@@ -9,13 +9,13 @@ class UnsupportedMethodHandler
 {
     public function handle($request, Closure $next)
     {
-        // try {
-        //     return $next($request);
-        // } catch (MethodNotAllowedHttpException $exception) {
-        //     // Jika metode tidak didukung, Anda dapat mengalihkan pengguna ke halaman lain atau menampilkan pesan kesalahan.
-        //     // Di sini, contoh redirect ke halaman tertentu:
-        //     return redirect()->route('ticket.index'); // Ganti dengan nama rute yang sesuai.
-        // }
+        try {
+            return $next($request);
+        } catch (MethodNotAllowedHttpException $exception) {
+            // Jika metode tidak didukung, Anda dapat mengalihkan pengguna ke halaman lain atau menampilkan pesan kesalahan.
+            // Di sini, contoh redirect ke halaman tertentu:
+            return redirect()->route('ticket.index'); // Ganti dengan nama rute yang sesuai.
+        }
     }
 }
 
