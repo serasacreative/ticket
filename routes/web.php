007d67afe202bb5de3ae0cef634bba26feab029c
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-08-01 07:11:32 +0700

    hja

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [TicketController::class, 'index'])->name('ticket.index');
-// Route::get('/ticket/festival', [TicketController::class, 'ticket_festival'])->name('ticket.festival');
-// Route::get('/ticket/vip', [TicketController::class, 'ticket_vip'])->name('ticket.vip');
-// Route::get('/ticket/generate/{id}', [TicketController::class, 'generate'])->name('ticket.generate');
 Route::get('/scan/eyJpdiI6IkVqZUQzTlhTN1pRalNUTmdIUkJXeWc9PSIsInZhbHVlIjoiRFRNYUFkdDdIMURaNzQwU0xYTnA5dz09IiwibWFjIjoiY2FkMDNjZWMxZTJjNTBiMTVjZDAwYTYzNjY2NTNlYzIyNzIxOGYyNTQyNjdjZDI4MTJmNjg5YmQwMzUzYjk3NiIsInRhZyI6IiJ9', [TicketController::class, 'scan'])->name('ticket.scan');
-// Route::get('/ticket/check/eyJpdiI6ImpuejNhbU9IN3pENjRIcldpckFZbXc9PSIsInZhbHVlIjoiMlpsZDJWTnBlLzFWRkk4S1RzMS9XQ2N5cEpTNi9BNzZiRjNWUC9MZ2xUaz0iLCJtYWMiOiJkMDgxN2Q1ZTg4ZDhjZWFlMWMzNWNjNDU3MjM5YTgzNGRkODA3NDk5YzM5Njk5NGJiZDFhOTFkZGU0YWRiYTBjIiwidGFnIjoiIn0', [TicketController::class, 'check'])->name('ticket.check');
 Route::get('/ticket/admin/eyJpdiI6ImpuejNhbU9IN3pENjRIcldpckFZbXc9PSIsInZhbHVlIjoiMlpsZDJWTnBlLzFWRkk4S1RzMS9XQ2N5cEpTNi9BNzZiRjNWUC9MZ2xUaz0iLCJtYWMiOiJkMDgxN2Q1ZTg4ZDhjZWFlMWMzNWNjNDU3MjM5YTgzNGRkODA3NDk5YzM5Njk5NGJiZDFhOTFkZGU0YWRiYTBjIiwidGFnIjoiIn0', [TicketController::class, 'admin'])->name('ticket.admin');
 Route::post('/ticket/verify', [TicketController::class, 'verify'])->name('ticket.verify');
-// Route::get('/ticket/invoice/{id}', [TicketController::class, 'invoice'])->name('ticket.invoice');
-// Route::get('/ticket/checkout', [TicketController::class, 'checkout'])->name('ticket.checkout');
-// Route::post('/checkout/festival', [TicketController::class, 'checkout_festival'])->name('ticket.checkout.festival');
-// Route::post('/checkout/vip', [TicketController::class, 'checkout_vip'])->name('ticket.checkout.vip');
-// Route::get('/email', [TicketController::class, 'email'])->name('email');
+
+Route::get('/ticket/generate/{id}', [TicketController::class, 'generate'])->name('ticket.generate');
