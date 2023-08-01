48d125cc93379ee8a0b27bfc5feaace79a291635
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-08-01 07:14:31 +0700

    soldou

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -38,8 +38,8 @@ class TicketController extends Controller
         })
         ->sum('qty');
         $max_tickets = env('MAX_FESTIVAL_TICKET');
+        return view('ticket.festival_soldout');
         if($tickets >= $max_tickets){
-            return view('ticket.festival_soldout');
         }
         $price = env('FESTIVAL_TICKET_PRICE');
         $is_presale = env('IS_PRESALE');
@@ -55,8 +55,8 @@ class TicketController extends Controller
         })
         ->sum('qty');
         $max_tickets = env('MAX_VIP_TICKET');
+        return view('ticket.vip_soldout');
         if($tickets >= $max_tickets){
-            return view('ticket.vip_soldout');
         }
         $price = env('VIP_TICKET_PRICE');
         $is_presale = env('IS_PRESALE');
