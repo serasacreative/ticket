e3673b546b82e4bb8262be8bb4889b500c8a029c
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-08-04 23:00:42 +0700

    p

--- app/Console/Commands/SendScheduledEmails.php
+++ app/Console/Commands/SendScheduledEmails.php
@@ -12,7 +12,33 @@ use Exception;
 class SendScheduledEmails extends Command
 {
     /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:send-scheduled';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send scheduled emails with a delay of 2 minutes';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
      * Execute the console command.
+     *
+     * @return void
      */
     public function handle()
     {
