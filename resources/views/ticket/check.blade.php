dfc5d1c12527ea8b4c8ad19b4426e969b6e1b2af
Muhammad Yunus <muhammadyunus1072@gmail.com>
2023-08-05 10:49:23 +0700

    table

--- resources/views/ticket/check.blade.php
+++ resources/views/ticket/check.blade.php
@@ -28,6 +28,32 @@
                     </div>
                 </div>
             </div>
+            <div class="col-12">
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <h6 class="card-title">Basic Example 1</h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table mb-0">
+                                <thead>
+                                    <tr>
+                                        <th>order id</th>
+                                        <th>order status</th>
+                                        <th>email</th>
+                                        <th>email status</th>
+                                        <th>category</th>
+                                        <th>qty</th>
+                                        <th>total price</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="orders-table">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
@@ -55,6 +81,26 @@
                 data: { data: inputData },
                 success: function (response) {
                     console.log(response)
+                    if (Array.isArray(response)) {
+                        var tableBody = '';
+                        response.forEach(function (data) {
+                            tableBody += '<tr>';
+                            tableBody += '<td>' + data.order.id + '</td>';
+                            tableBody += '<td>' + data.order.status + '</td>';
+                            tableBody += '<td>' + data.email + '</td>';
+                            tableBody += '<td>' + data.status + '</td>';
+                            tableBody += '<td>' + data.order.category + '</td>';
+                            tableBody += '<td>' + data.order.qty + '</td>';
+                            tableBody += '<td>' + data.order.total_price + '</td>';
+                            tableBody += '</tr>';
+                        });
+
+                        // Assuming your table has an ID "orders-table" and a tbody with ID "table-body"
+                        $('#orders-table').html(tableBody);
+                    } else {
+                        // Handle the case when the response is not an array or doesn't contain the expected data.
+                        $('#result').html('<p>Error in response data format.</p>');
+                    }
                 },
                 error: function (xhr, status, error) {
                     console.error(xhr.responseText);
